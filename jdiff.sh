afb1e6b2867d375210fc3377b45443d0a878a2ac
zfolwick <zfolwick@gmail.com>
2025-04-12 11:33:33 -0700

    Update jdiff.sh

--- jdiff.sh
+++ jdiff.sh
@@ -5,15 +5,29 @@ file1="${@:(-2):1}"
 
 # some objects we'll want to ignore
 ignorefile_opt="${@:(-3):1}"
-if [ ! -z $ignorefile_opt ]; then
-  pattern="*="
-  ignorefile="${ignorefile_opt#*$pattern}"
+ignorefile=''
+if [ ! -f $ignorefile_opt ]; then
+
+  if [[ $ignorefile_opt =~ "ignorefile" ]]; then
+    # it's an ignorefile option
+    pattern="*="
+    ignorefile="${ignorefile_opt#*$pattern}"
+    diff_opt_num="$(($#-3))"
+  else
+    # it's a diff option
+    diff_opt_num="$(($#-2))"
+  fi
 fi
-ignore_lines=$(sed -e '$!s/$/?, /' -e '$s/$/?/' -e 's/^/./' $ignorefile | tr -d '\n')
+
+if [ $ignorefile ]; then
+  ignore_lines=$(sed -e '$!s/$/?, /' -e '$s/$/?/' -e 's/^/./' $ignorefile | tr -d '\n') || ''
+fi
+
+if [[ -n $ignore_lines ]]; then
+  jqscript="walk(if type == \"array\" then sort else . end) | del(.. | ${ignore_lines})"
+else
+  jqscript="walk(if type == \"array\" then sort else . end)"
+fi
 
 
-diff_options=$(($#-3))
-
-jqscript="walk(if type == \"array\" then sort else . end) | del(.. | ${ignore_lines})"
-
-diff ${@:1:$diff_options} <(jq --sort-keys "${jqscript}" < "$file1") <(jq --sort-keys "${jqscript}" < "$file2")
+diff ${@:1:$diff_opt_num} <(jq --sort-keys "${jqscript}" < "$file1") <(jq --sort-keys "${jqscript}" < "$file2")
